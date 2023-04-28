142b47b3042d3efbcc3920dd2797d03ba529bdc6
zbass90 <smilejkh90@gmail.com>
2023-04-28 16:58:04 +0900

    spring jpa Initail Setting

---
+++ src/main/java/study/datajpa/entity/Member.java
@@ -0,0 +1,37 @@
+package study.datajpa.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import static jakarta.persistence.FetchType.LAZY;
+
+@Entity
+@Getter @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString(of = {"id","username","age"}) // 연관관계 필드는 toString에서 빼라
+public class Member {
+    @Id @GeneratedValue
+    @Column(name = "member_id")
+    private Long id;
+    private String username;
+    private int age;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "team_id")
+    private Team team;
+
+    public Member(String username) {
+        this.username = username;
+    }
+
+    public Member(String username, int age, Team team) {
+        this.username = username;
+        this.age = age;
+        if(team != null) changeTeam(team);
+    }
+
+    public void changeTeam(Team team){
+        this.team = team;
+        team.getMembers().add(this);
+    }
+}
